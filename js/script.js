6534d8627e3e181434876cd0e2a5a76de0d1b4bd
unknown <5008.oshimanatsumi@gmail.com>
2026-03-09 00:16:09 +0900

    add imge and anime

--- js/script.js
+++ js/script.js
@@ -7,7 +7,14 @@ btn.addEventListener("click", () => {
 
 const hamburger = document.querySelector(".js-hamburger");
 const nav = document.querySelector(".js-nav");
+const main = document.querySelector("main");
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("open");
+    hamburger.classList.toggle("active");
+    if(nav.classList.contains("open")) {
+        main.style.transform = "translateY(50px)";
+    } else {
+        main.style.transform = "translateY(0)";
+    }
 });
